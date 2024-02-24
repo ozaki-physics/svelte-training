6f1977fc042ec4cd37f03f237d8d5a0e35eb1283
ozaki-physics <setuna.f.masato@gmail.com>
2024-02-24 23:25:04 +0900

    feat: チュートリアル: layout.server.js の仕組み

--- app/src/routes/blog/[slug]/+layout.svelte
+++ app/src/routes/blog/[slug]/+layout.svelte
@@ -1,7 +1,22 @@
+<script>
+  export let data;
+</script>
+
 <div class="layout">
   <main>
     <slot />
   </main>
+
+  <aside>
+    <h2>More posts</h2>
+    <ul>
+      {#each data.summaries as { slug, title }}
+        <li>
+          <a href="/blog/{slug}">{title}</a>
+        </li>
+      {/each}
+    </ul>
+  </aside>
 </div>
 
 <style>
