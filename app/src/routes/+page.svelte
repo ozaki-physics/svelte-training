67066da3fac275e7addc446a8cfca6837220249e
ozaki-physics <setuna.f.masato@gmail.com>
2024-02-28 21:35:59 +0900

    feat: 共通で使うファイルの配置

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+  import { message } from "$lib/message.js";
+</script>
+
 <h1>home</h1>
 <p>this is the home page.</p>
+<p>{message}</p>
---
+++ app/src/routes/deeply/nested/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { message } from "$lib/message.js";
+</script>
+
+<h1>deeply nested</h1>
+<p>階層が深くても 同じように static にアクセスできるか</p>
+<p>{message}</p>
